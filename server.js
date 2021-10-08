c01908da7ff66686a3759859eeea470cf639d70c
ArturoZ5 <azarateg1900@alumno.ipn.mx>
2021-10-07 22:49:59 -0500

    subir archivos

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const mimeTypes = require('mime-types');
+
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: function(req,file,cb){
+        cb("",file.originalname);
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
 
 app.get("/",(req,res)=>{
  res.sendFile(__dirname + "/views/index.html");
 })
 
-app.post("/files",(req, res)=>{
+app.post("/files",upload.single('avatar') ,(req, res)=>{
+    res.send("Enviado correctamente");
 
 })
 
